b09c3bacb9e40be9c7d97a318c301354c5ca68c3
Joel-HackMaster <122944694+Joel-HackMaster@users.noreply.github.com>
2023-01-30 18:33:47 -0500

    Add files via upload

---
+++ JavaDesdeCero.java
@@ -0,0 +1,18 @@
+package com.javasesion1;
+
+public class JavaDesdeCero {
+    public static void main(String[] args) {
+        //Primitivos
+        int n = 8;
+        Double m = 8.7;
+        long n1 = (long)4.5485;
+        boolean g = true;
+        String nombre = "Juan";
+
+        System.out.println(n);
+        System.out.println(m);
+        System.out.println(n1);
+        System.out.println(g);
+        System.out.println(nombre);
+    }
+}
